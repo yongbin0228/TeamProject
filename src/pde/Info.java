cad2d60aa3d6b71352da6beb79ce9229c93a1263
yongbin0228 <bin3525@naver.com>
2019-06-07 18:59:26 +0900

    version 1.1

--- src/pde/Info.java
+++ src/pde/Info.java
@@ -24,22 +24,22 @@ class Info {
                 JOptionPane.showMessageDialog(null, "100CASH를 주웠습니다", "깜짝 보상", JOptionPane.INFORMATION_MESSAGE);
             }
         }
-        money+= coupon_money;
+        money += coupon_money;
     }
 
     static void exp_up() {
         if (level < 99) {
-            int random = (int)(Math.random()*100+1);
-            if(random == 77){
+            int random = (int) (Math.random() * 100 + 1);
+            if (random == 77) {
                 cash += 100;
-                JOptionPane.showMessageDialog(null,"100CASH를 주웠습니다", "깜짝 보상", JOptionPane.INFORMATION_MESSAGE);
+                JOptionPane.showMessageDialog(null, "100CASH를 주웠습니다", "깜짝 보상", JOptionPane.INFORMATION_MESSAGE);
             }
             exp += coupon_exp;
             if (exp >= max_exp) {
                 level++;
                 exp = 0;
-                money += level*10;
-                JOptionPane.showMessageDialog(null,"LEVEL UP!!\n돈  "+level*10+"원 획득", "레벨 업", JOptionPane.INFORMATION_MESSAGE);
+                money += level * 10;
+                JOptionPane.showMessageDialog(null, "LEVEL UP!!\n돈  " + level * 10 + "원 획득", "레벨 업", JOptionPane.INFORMATION_MESSAGE);
                 if (level % 10 == 0) {
                     add_exp += 10;
                 }
@@ -69,17 +69,20 @@ class Info {
 
     static void cash_add() {
 
-        While : while (true) {
+        While:
+        while (true) {
             input = JOptionPane.showInputDialog("충전 금액을 입력하세요 [ 1000, 5000, 10000, 50000 ] ");
             if (!(input.equals("1000") || input.equals("5000") || input.equals("10000") || input.equals("50000"))) {
                 JOptionPane.showMessageDialog(null, "금액이 올바르지 않습니다", "캐쉬 충전", JOptionPane.WARNING_MESSAGE);
             } else {
-                while(true) {
+                while (true) {
                     input2 = JOptionPane.showInputDialog("충전 코드를 입력하세요 ex) 1234-5678-1234-5678 ");
-                    if (!(input2.charAt(4) == '-' && input2.charAt(9) == '-' && input2.charAt(14) == '-')) {
+                    if ((input2.equals("2019-0605-2019-0607"))) {
+                        cash += Integer.valueOf(input);
+                        break While;
+                    } else {
                         JOptionPane.showMessageDialog(null, "코드가 올바르지 않습니다", "캐쉬 충전", JOptionPane.WARNING_MESSAGE);
-                    }else{
-                        cash+=Integer.valueOf(input);
+                        cash += Integer.valueOf(input);
                         break While;
                     }
                 }
@@ -87,3 +90,4 @@ class Info {
         }
     }
 }
+
